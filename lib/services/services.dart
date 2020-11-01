428247b7b52dbc94e874b145544d3ae3cac8f3ac
Nurcholis Rio <colisrio@gmail.com>
2020-11-01 22:35:20 +0700

    add sign up services api in user services

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:plant_market_flutter/shared/shared.dart';
+
 import '../models/models.dart';
+import 'package:http/http.dart' as http;
 
 part 'api_return_value.dart';
 part 'plant_services.dart';
